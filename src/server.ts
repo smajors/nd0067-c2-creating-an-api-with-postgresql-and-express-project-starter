eab97286fa33abcc180187a5278e4d16a7dc6a08
Stephen Majors <stephen@majorscreations.com>
2022-04-26 01:14:33 -0500

    Added eslint to project and ran first lint.

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 
 const app: express.Application = express();
-const address: string = '0.0.0.0:3000';
+const address = '0.0.0.0:3000';
 
 app.use(bodyParser.json());
 
