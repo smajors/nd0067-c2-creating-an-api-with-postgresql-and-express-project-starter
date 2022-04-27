27460fac719b627333ea23888628ccf8f28eeb56
Stephen Majors <stephen@majorscreations.com>
2022-04-27 16:09:04 -0500

    Added basic CORS support

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,18 @@
 import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors'
 
 const app: express.Application = express();
 const address = '0.0.0.0:3000';
+
+// CORS options
+const corsOptions = {
+  origin: 'http://127.0.0.1',
+  optionsSuccessStatus: 200
+}
+
+// Use CORS
+app.use(cors(corsOptions));
 
 app.use(bodyParser.json());
 
