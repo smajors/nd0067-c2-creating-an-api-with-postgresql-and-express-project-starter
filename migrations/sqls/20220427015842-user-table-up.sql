23caf13ef456812db744d5463c5947b42a567dbb
Stephen Majors <stephen@majorscreations.com>
2022-04-27 22:43:03 -0500

    Added username to user table

--- migrations/sqls/20220427015842-user-table-up.sql
+++ migrations/sqls/20220427015842-user-table-up.sql
@@ -1,7 +1,8 @@
 /* Create user table */
 CREATE TABLE site_user 
 (
-    id SERIAL PRIMARY KEY, 
+    id SERIAL PRIMARY KEY,
+    user_name VARCHAR(25) UNIQUE NOT NULL, 
     first_name VARCHAR(50) NOT NULL, 
     last_name VARCHAR(50) NOT NULL, 
     password CHAR(64) NOT NULL
