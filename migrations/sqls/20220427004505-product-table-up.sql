23f2773c2a2fe54298eb54c542276e5ba1ce082f
Stephen Majors <stephen@majorscreations.com>
2022-05-07 21:48:27 -0500

    Fixed table up sql to correct numeric type

--- migrations/sqls/20220427004505-product-table-up.sql
+++ migrations/sqls/20220427004505-product-table-up.sql
@@ -1,4 +1,4 @@
 /* Create Category Table as prerequsite for Product Table */
 CREATE TABLE category (id SERIAL PRIMARY KEY, name VARCHAR(50) NOT NULL);
 /* Create product table */
-CREATE TABLE product (id SERIAL PRIMARY KEY, name VARCHAR(100) NOT NULL, price NUMERIC(2) NOT NULL, category_id INTEGER NOT NULL REFERENCES category (id));
+CREATE TABLE product (id SERIAL PRIMARY KEY, name VARCHAR(100) NOT NULL, price NUMERIC(10,2) NOT NULL, category_id INTEGER REFERENCES category (id));
